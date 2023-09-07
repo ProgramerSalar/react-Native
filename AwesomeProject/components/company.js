bbbfbe455b0709f28fac294dd8186f5374bef0c2
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-07 15:00:53 +0530

    state and props in class components

--- AwesomeProject/components/company.js
+++ AwesomeProject/components/company.js
@@ -1,23 +1,48 @@
 import React, { useState, Component } from 'react';
 import { View, Text, StyleSheet, TextInput, Button, FlatList, ScrollView, SectionList } from 'react-native';
 import { styles } from './style';
+import Student from './student';
 
 
 
 
 class Manish extends Component{
-    fruit = () => {
-        console.warn('Apple')
+    constructor(){
+        super()
+        this.state={
+            name:"Anil",
+            age:undefined,
+        }
     }
+
+
+    updateName(val){
+        this.setState({name:val})
+    }
+
+
+
     render(){
         return(
             <View>
-                <Text>Hello</Text>
-                <Button title='ok' onPress={this.fruit}></Button>
+                <Text>{this.state.name}{this.state.age}</Text>
+                <TextInput onChangeText={(text) => this.updateName(text)}></TextInput>
+                <Button title='ok' ></Button>
+                <Student name={this.state.name}/>
             </View>
         )
     }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 export default Manish;
---
+++ AwesomeProject/components/student.js
@@ -0,0 +1,19 @@
+import React, { useState, Component } from 'react';
+import { View, Text, StyleSheet, TextInput, Button, FlatList, ScrollView, SectionList } from 'react-native';
+
+
+
+
+class Student extends Component{
+    render(){
+
+       
+        return(
+            <View>
+                <Text>Student Name: {this.props.name} </Text>
+            </View>
+        )
+    }
+}
+
+export default Student;
