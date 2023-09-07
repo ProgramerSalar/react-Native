518c33b9e4a667ae3e205e77c31d4f5a12ecb5b7
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-07 20:31:49 +0530

    TouchableHighlight button

--- AwesomeProject/components/company.js
+++ AwesomeProject/components/company.js
@@ -1,45 +1,47 @@
 import React, { useState, Component, useEffect } from 'react';
-import { View, Text, StyleSheet, TextInput, Button, FlatList, ScrollView, SectionList } from 'react-native';
-import { styles } from './style';
+import { View, Text, StyleSheet, TextInput, Button, FlatList, ScrollView, SectionList, TouchableHighlight } from 'react-native';
+// import { styles } from './style';
 import Student from './student';
 
 
 
-
-
 export const Manish = () => {
-    const [show, setShow] = useState(true)
-
-    return(
+    return (
         <View>
             <Text>Hello</Text>
-            <Button title='Tongle' onPress={() => setShow(!show)}></Button>
-            {
-                show ? <User/> : null
-            }
-            
+            <TouchableHighlight>
+                <Text style={[styles.button, styles.sucess]}>Button</Text>
+            </TouchableHighlight>
+           
         </View>
     )
 }
 
 
-const User = () => {
 
-    let timer = setInterval(() => {
-        console.warn('Timer')
 
-    },2000)
+const styles = StyleSheet.create({
 
-    useEffect(() => 
-    {
-        return()=> clearInterval(timer)
+    button:{
+        backgroundColor:'red',
+        color:'white',
+        fontSize:24,
+        textAlign:'center',
+        padding:10,
+        margin:10,
+        borderRadius:10,
+        // shadowColor:'yellow',
+        // elevation:10,
+        // shadowOpacity:1
+    }, 
+    sucess:{
+        backgroundColor:'green',
+        color:'white',
+        fontSize:24,
+        textAlign:'center',
+        padding:10,
+        margin:10,
+        borderRadius:10,
+        
     }
-    )
-
-    return(
-        <View>
-            <Text>Student</Text>
-        </View>
-    )
-
-}
+})
