2f970b96cdc431a30c952acfb6886406d7f362bc
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-07 14:25:29 +0530

    Class in react native

--- AwesomeProject/components/company.js
+++ AwesomeProject/components/company.js
@@ -1,66 +1,23 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button, FlatList, ScrollView } from 'react-native';
+import React, { useState, Component } from 'react';
+import { View, Text, StyleSheet, TextInput, Button, FlatList, ScrollView, SectionList } from 'react-native';
 import { styles } from './style';
 
 
-export const Manish = () => {
 
-    const users = [
-        {
-            id: 1,
-            name: 'manish',
-        },
-        {
-            id: 2,
-            name: 'manish',
-        },
-        {
-            id: 3,
-            name: 'manish',
-        },
-        {
-            id: 4,
-            name: 'manish',
-        },
-        {
-            id: 5,
-            name: 'manish',
-        },
 
-    ]
-    return (
-        <View>
-            <FlatList
-                data={users}
-                renderItem={({ item }) => <UserData item={item}  /> }
-            />
-        </View>
-
-    )
+class Manish extends Component{
+    fruit = () => {
+        console.warn('Apple')
+    }
+    render(){
+        return(
+            <View>
+                <Text>Hello</Text>
+                <Button title='ok' onPress={this.fruit}></Button>
+            </View>
+        )
+    }
 }
 
 
-
-
-
-const UserData = (props) => {
-    const item = props.item 
-    return (
-        <View>
-            
-            <Text>{item.name}</Text>
-            
-        </View>
-    )
-}
-
-const ID = (props) => {
-    const item = props.item 
-    return (
-        <View>
-            
-            <Text>{item.name}</Text>
-
-        </View>
-    )
-}
+export default Manish;
