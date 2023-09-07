5729a6614fcd827ecb04afa31be2b7c2c4d5464a
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-07 19:12:00 +0530

    useEffect hook as ComponentDidUpdate life cycle method

--- AwesomeProject/components/company.js
+++ AwesomeProject/components/company.js
@@ -6,18 +6,50 @@ import Student from './student';
 
 export const Manish = () => {
     const [count, setCount] = useState(0)
-    useEffect(() =>{
-        console.warn('hello')
-    }, [] // bracket is life cycles 
-        
-    )
-    return (
-        <View>
-            <Text>
-                Hello {count}
-            </Text>
+    const [data, setData] = useState(100)
 
-            <Button title='update Count' onPress={() => setCount(count+1)}></Button>
+
+    
+    // useEffect(() => {
+    //     console.warn(count)
+    //     console.warn(data)
+       
+    // },[count, data]);
+
+
+    // useEffect(() => {
+    //     console.warn(data)
+    // },[data])
+
+
+
+
+
+
+
+
+    return(
+        <View>
+            <Text>Hello:{count}, Data:{data}</Text>
+            <Button title='ok' onPress={() => setCount(count+1)}></Button>
+            <Button title='ok' onPress={() => setData(data+1)}></Button>
+
+            <User info={{data, count}}/>
+        </View>
+    )
+}
+
+
+
+const User = (props) => {
+    useEffect(() => {
+        console.warn(props.info)
+    },[props.info.data, props.info.count])
+
+    return(
+        <View>
+            <Text>data: {props.info.data}</Text>
+            <Text>count:{props.info.count} </Text>
         </View>
     )
 }
