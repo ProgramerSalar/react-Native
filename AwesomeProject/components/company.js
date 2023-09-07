48bc2deae381349b6e721c65591b31a285bbfd98
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-07 19:32:43 +0530

    Togle, show and hide component

--- AwesomeProject/components/company.js
+++ AwesomeProject/components/company.js
@@ -4,52 +4,36 @@ import { styles } from './style';
 import Student from './student';
 
 
+
+
+
+
 export const Manish = () => {
-    const [count, setCount] = useState(0)
-    const [data, setData] = useState(100)
-
-
-    
-    // useEffect(() => {
-    //     console.warn(count)
-    //     console.warn(data)
-       
-    // },[count, data]);
-
-
-    // useEffect(() => {
-    //     console.warn(data)
-    // },[data])
-
-
-
-
-
-
-
+    const [hide, hideState] = useState(false)
 
     return(
         <View>
-            <Text>Hello:{count}, Data:{data}</Text>
-            <Button title='ok' onPress={() => setCount(count+1)}></Button>
-            <Button title='ok' onPress={() => setData(data+1)}></Button>
-
-            <User info={{data, count}}/>
+            <Text>On Hide </Text>
+            {/* <Button title='Hide' onPress={() => hideState(false)}></Button>
+            <Button title='Show' onPress={() => hideState(true)}></Button> */}
+            <Button title='Tongle Component' onPress={() => hideState(!hide)}></Button>
+            
+            {
+                hide ? <User/> : null
+            }
+        
+            
         </View>
     )
 }
 
 
 
-const User = (props) => {
-    useEffect(() => {
-        console.warn(props.info)
-    },[props.info.data, props.info.count])
-
+const User = () => {
     return(
         <View>
-            <Text>data: {props.info.data}</Text>
-            <Text>count:{props.info.count} </Text>
+            <Text>Hide components</Text>
+
         </View>
     )
 }
