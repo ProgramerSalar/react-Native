1f945db3ef643771e5dc4e3bb2e55868f887bb32
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-09 12:44:14 +0530

    Flatlist with Api data

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -1,44 +1,43 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { View, Text, Button, StyleSheet, Modal, Pressable, StatusBar, Platform, TextInput, ScrollView } from 'react-native';
+import { View, Text, Button, StyleSheet, Modal, Pressable, StatusBar, Platform, TextInput, ScrollView, FlatList } from 'react-native';
 // import { Manish } from './components/company';
 
 
 export const App = () => {
-
   const [data, setData] = useState([])
 
   const getAPIData = async() => {
-    const url="https://jsonplaceholder.typicode.com/posts/"
+    // console.warn('hello')
+    const url = 'https://jsonplaceholder.typicode.com/posts/'
     let result = await fetch(url)
     result = await result.json()
     setData(result)
-  }
 
+  }
 
   useEffect(() => {
     getAPIData()
-  },[])
-
+  })
 
   return(
-    <ScrollView>
-      <Text style={{fontSize:40}}>
-        List with API Call
-      </Text>
+    <View>
+      <Text>FlatList with Api data</Text>
       {
         data.length ? 
-        data.map((item) => 
-        
-            <View>
-            <Text style={{fontSize:30}}>ID: {item.id}</Text>
-            <Text style={{fontSize:10}}>ID: {item.title}</Text>
+        <FlatList 
+        data = {data}
+        renderItem={({item}) => 
 
-            </View>
-           
-          
-        )
-        :null
+        <View>
+          <Text style={{fontSize:20}}>ID:{item.id}</Text>
+          <Text style={{fontSize:15}}>Title: {item.title}</Text>
+          <Text style={{fontSize:10}}>BOdy: {item.body}</Text>
+        </View>
+      
       }
-    </ScrollView>
+        /> 
+        : null
+      }
+    </View>
   )
 }
