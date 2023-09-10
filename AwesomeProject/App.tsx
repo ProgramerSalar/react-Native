89ff28e9681df8c7c39bbcc60d59d41eadd509f1
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-10 10:49:22 +0530

    Simple Form Validators

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -10,8 +10,48 @@ export const App =  () => {
     const [age, setAge] = useState(0)
     const [email, setEmail] = useState('')
 
+
+    const [nameError, setNameError] = useState(false)
+    const [ageError, setAgeError] = useState(false)
+    const [emailError, setEmailError] = useState(false)
+
+
+
+    
+
     const saveData = async () => {
-        console.warn(name,age,email)
+        
+        if(!name){
+            setNameError(true)
+        }else{
+            setNameError(false)
+        }
+
+        if(!age){
+            setAgeError(true)
+        }else{
+            setAgeError(false)
+        }
+
+        if(!email){
+            setEmailError(true)
+        }else{
+            setEmailError(false)
+        }
+
+        
+
+        if(!name || !age || !email){
+            return false
+        }
+
+
+        console.warn("next")
+
+
+
+
+        // console.warn(name,age,email)
 
         const url = "http://10.0.2.2:3000/users"
         let results = await fetch(url, {
@@ -45,8 +85,17 @@ export const App =  () => {
         <View>
             <Text style={{fontSize:40, alignItems:'center', textAlign:'center'}}>Form</Text>
             <TextInput placeholder='Enter Name' style={styles.input} value={name} onChangeText={(text) => setName(text)}></TextInput>
+            {
+                nameError ? <Text style={{marginLeft:20, color:'red'}}>Please Enter Valid Name</Text>:null
+            }
             <TextInput placeholder='Enter Age' style={styles.input} value={age} onChangeText={(text) => setAge(text)}></TextInput>
+            {
+                ageError ? <Text style={{marginLeft:20, color:'red'}}>Please Enter Valid age</Text>:null
+            }
             <TextInput placeholder='Enter Email' style={styles.input} value={email} onChangeText={(text) => setEmail(text)}></TextInput>
+            {
+                emailError ? <Text style={{marginLeft:20, color:'red'}}>Please Enter Valid Email</Text>:null
+            }
             <Button title='Save Data' onPress={saveData}></Button>
             
             
@@ -62,5 +111,6 @@ const styles = StyleSheet.create({
         borderWidth:1,
         margin:20,
         borderRadius:10,
+        marginBottom:2
     }
 })
