6bb80c109b40432f53e76aeb6638b048e1191c58
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-11 10:47:37 +0530

    #9 Remove To cart UI

--- AwesomeProject/components/Product.js
+++ AwesomeProject/components/Product.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState, useRef } from 'react';
 import { View, Image, Text, Button, StyleSheet, Modal, Pressable, StatusBar, Platform, TextInput, ScrollView, FlatList, Alert } from 'react-native';
 import { addToCart } from './redux/action';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 
 
@@ -9,22 +9,44 @@ import { useDispatch } from 'react-redux'
 export const Product = (props) => {
     const item = props.item
     const dispatch = useDispatch()
+    const cartItems = useSelector((state) => state.reducer)
+    const [isAdded, setIsAdded] = useState(false)
+
+
 
     const handleAddTOCart =(item) => {
         console.warn('Hello', item)
         dispatch(addToCart(item))
-        
-
-
-
     }
+
+
+    useEffect(() => {
+        if (cartItems && cartItems.length){
+            cartItems.forEach((element) =>{
+                // console.warn(element)
+                if(element.name === item.name){
+                    setIsAdded(true)
+                }
+            } )
+        }
+    },[cartItems])
+
+
+
     return (
         <View style={{alignItems:"center", borderBottomColor:'black', borderBottomWidth:2, padding:50}}>
             <Text style={{ fontSize: 20 }}>{item.name}</Text>
             <Text style={{ fontSize: 20 }}>{item.price}</Text>
             <Text style={{ fontSize: 20 }}>{item.color}</Text>
             <Image style={{ width: 100, height: 100 }} source={{ uri: item.image }}></Image>
-            <Button title='Add to Cart' onPress={() => handleAddTOCart(item)}></Button>
+            {
+                isAdded?
+                <Button title='Remove To Cart' onPress={() => handleAddTOCart(item)}></Button>
+                :
+                <Button title='Add to Cart' onPress={() => handleAddTOCart(item)}></Button>
+
+            }
+            
         </View>
     )
 
