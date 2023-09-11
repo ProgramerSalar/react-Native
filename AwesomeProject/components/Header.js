9859b58f6c4a11275f7fe71e196c1a265da2f769
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-11 10:37:21 +0530

    Get data from Redux store

--- AwesomeProject/components/Header.js
+++ AwesomeProject/components/Header.js
@@ -1,13 +1,27 @@
 import React, { useEffect, useMemo, useState, useRef } from 'react';
 import { View, Text, Button, StyleSheet, Modal, Pressable, StatusBar, Platform, TextInput, ScrollView, FlatList, Alert } from 'react-native';
 
-
+import {useSelector} from 'react-redux'
 
 
 export const Header = () => {
+
+    const cartData = useSelector((state) => state.reducer)
+    const [cartItems, setCartItems] = useState(0)
+    // console.warn(cartData)
+
+
+    useEffect(() => {
+        setCartItems(cartData.length)
+
+
+    },[cartData])
+
+
+
     return(
         <View>
-            <Text style={{fontSize:30, textAlign:'right', padding:10, backgroundColor:'orange'}}>0</Text>
+            <Text style={{fontSize:30, textAlign:'right', padding:10, backgroundColor:'orange'}}>{cartItems}</Text>
         </View>
     )
 
