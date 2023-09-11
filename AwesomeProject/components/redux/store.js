a1ff92aa723b7c45522ab6e6a8683e0c653cc4c3
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-11 10:04:43 +0530

    Store and Provider

---
+++ AwesomeProject/components/redux/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit'
+import rootReducer from './rootReducer'
+
+
+
+export const store = configureStore({
+    reducer:rootReducer
+})
+
+
--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -6,4 +6,20 @@ import {AppRegistry} from 'react-native';
 import {App} from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+
+// import 
+import { Provider } from 'react-redux';
+import { store } from './components/redux/store';
+
+
+
+const AppRedux = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+
+
+
+AppRegistry.registerComponent(appName, () => AppRedux);
