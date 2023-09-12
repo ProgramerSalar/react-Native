75d002312c763a192e09232abffcddafe08a14d2
ProgramerSalar <manishkumar60708090@gmail.com>
2023-09-12 10:27:27 +0530

    #9 Setup and configure saga

--- AwesomeProject/components/redux/store.js
+++ AwesomeProject/components/redux/store.js
@@ -2,9 +2,19 @@ import {configureStore} from '@reduxjs/toolkit'
 import rootReducer from './rootReducer'
 
 
+// saga 
+import createSagaMiddleware from 'redux-saga'
+import { sagaData } from './saga'
+
+
+
+
+const sagaMiddleware = createSagaMiddleware()
 
 export const store = configureStore({
-    reducer:rootReducer
+    reducer:rootReducer,
+    middleware:()=> [sagaMiddleware]
 })
 
 
+sagaMiddleware.run(sagaData)
